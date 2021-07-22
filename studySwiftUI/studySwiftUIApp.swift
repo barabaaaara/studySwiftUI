b1dd34b8952a97542ee83d256a1ced8e931a8f04
Yusuke KUWAHARA <barabaaaara0127@gmail.com>
2021-07-22 21:37:24 +0900

    Initial Commit

---
+++ studySwiftUI/studySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  studySwiftUIApp.swift
+//  studySwiftUI
+//
+//  Created by 桑原佑輔 on 2021/07/22.
+//
+
+import SwiftUI
+
+@main
+struct studySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
