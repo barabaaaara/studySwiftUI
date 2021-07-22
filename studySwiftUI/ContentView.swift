f4b0a01e817a986b8b987ef27acf60072222d91f
Yusuke KUWAHARA <barabaaaara0127@gmail.com>
2021-07-22 22:12:44 +0900

    study text field and @State

--- studySwiftUI/ContentView.swift
+++ studySwiftUI/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = "Hello, world!"
+    @State var newItem: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(text)
+            TextField("New Item", text: $newItem, onCommit:{//初期値が”New Item”でvar newItemと紐付いているテキストフィールド。編集後に反映される。
+                self.text = self.newItem //編集完了後に変数textに変数newItemを代入
+                self.newItem = ""//変数newItemを空にする
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())//外枠のついたテキストフィールド
+            .frame(width: 300)//外枠の長さ
+        }
+        
+        
     }
 }
 
